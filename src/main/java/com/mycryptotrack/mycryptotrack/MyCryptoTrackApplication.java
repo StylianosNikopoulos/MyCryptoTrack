21983e371e7b502e9d827b8fdcb36a9ef55f04ad
StylianosNikopoulos <nikstel2001@gmail.com>
2025-09-21 13:34:49 +0300

    First Commit

---
+++ src/main/java/com/mycryptotrack/mycryptotrack/MyCryptoTrackApplication.java
@@ -0,0 +1,13 @@
+package com.mycryptotrack.mycryptotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCryptoTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCryptoTrackApplication.class, args);
+	}
+
+}
