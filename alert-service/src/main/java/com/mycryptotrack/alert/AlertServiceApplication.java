ddf67530d091bd56cfd376725841786c7376f2bf
StylianosNikopoulos <nikstel2001@gmail.com>
2025-09-23 19:33:35 +0300

    Setup Swagger

---
+++ alert-service/src/main/java/com/mycryptotrack/alert/AlertServiceApplication.java
@@ -0,0 +1,11 @@
+package com.mycryptotrack.alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class AlertServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlertServiceApplication.class, args);
+    }
+}
---
+++ market-ingest-service/src/main/java/com/mycryptotrack/market/App.java
@@ -0,0 +1,11 @@
+package com.mycryptotrack.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MarketIngestServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarketIngestServiceApplication.class, args);
+    }
+}
