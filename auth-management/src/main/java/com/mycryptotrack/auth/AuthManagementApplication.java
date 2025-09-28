1ff9ada8e47b91a1ad867a350b4b15b7e52c8f45
StylianosNikopoulos <nikstel2001@gmail.com>
2025-09-28 13:54:14 +0300

    Rename microservices

--- auth-management/src/main/java/com/mycryptotrack/auth/AuthManagementApplication.java
+++ auth-management/src/main/java/com/mycryptotrack/auth/AuthManagementApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class AuthManagementApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
+        SpringApplication.run(AuthManagementApplication.class, args);
     }
 }
